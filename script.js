fb711bb5480ed99e96268090cad02eac78ee9a16
Carol Vendramini <caroline.vendramini07@gmail.com>
2022-04-05 19:09:00 -0300

    adicionando script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("TESTE")
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@";
+let novaSenha = "";
+
+sizePassword.innerHTML =sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i <  sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;  
+    novaSenha = pass;
+
+}
+
+function copyPassword(){
+    navigator.clipboard.writeText(novaSenha);
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Senha copiada com sucesso!',
+        showConfirmButton: false,
+        timer: 1500
+    });
+
+}
